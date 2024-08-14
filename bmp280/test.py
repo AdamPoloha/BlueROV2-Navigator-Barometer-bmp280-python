2e8378b81d5fdc0e167f76d5f149b61a52fad164
AdamPoloha <96154207+AdamPoloha@users.noreply.github.com>
2024-08-15 10:54:56 +1200

    Simple test with print instead of log

--- bmp280/test.py
+++ bmp280/test.py
@@ -2,23 +2,20 @@
 
 def main():
     from bmp280 import BMP280
-    from llog import LLogWriter
 
     device = "bmp280"
-    parser = LLogWriter.create_default_parser(__file__, device)
-    parser.add_argument("--bus", default=1, help="i2c bus")
-    args = parser.parse_args()
+    bmp = BMP280()
 
-    with LLogWriter(args.meta, args.output, console=args.console) as log:
-        bmp = BMP280(args.bus)
-        compensation = bmp.get_compensation()
-        log.log_rom(' '.join(str(d) for d in compensation.data))
-        log.log_config(f'{bmp.osrs_t} {bmp.osrs_p} {bmp.mode} {bmp.t_sb} {bmp.filter}')
+    while True:
+        #compensation = bmp.get_compensation()
 
         def data_getter():
             data = bmp.get_data()
-            return f'{data.pressure:.6f} {data.temperature:.6f} {data.pressure_raw} {data.temperature_raw}'
-        log.log_data_loop(data_getter, parser_args=args)
+            #print(f'{data.pressure:.6f} {data.temperature:.6f} {data.pressure_raw} {data.temperature_raw}')
+            print(data.pressure, data.temperature, data.pressure_raw, data.temperature_raw)
+            #bmp.close()
+        
+        data_getter()
 
 if __name__ == '__main__':
     main()
